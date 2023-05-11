e900d569c5fb47eb40d70529d46860f29697c609
ARTHUR VERDADEIRO <arthur.verdadeiro@etec.sp.gov.br>
2023-05-11 17:08:09 -0300

    first commit

---
+++ crudTest/resources/views/students/create.blade.php
@@ -0,0 +1,22 @@
+@extends('students.frame')
+@section('content')
+  
+<div class="card" style="margin:20px;">
+  <div class="card-header">Criar novos ESTUDANTES</div>
+  <div class="card-body">
+       
+      <form action="{{ url('/estudantes') }}" method="post">
+        {!! csrf_field() !!}
+        <label>Nome</label></br>
+        <input type="text" name="name" id="name" class="form-control"></br>
+        <label>Email</label></br>
+        <input type="text" name="email" id="email" class="form-control"></br>
+        <label>Celular</label></br>
+        <input type="text" name="phone" id="phone" class="form-control"></br>
+        <input type="submit" value="Save" class="btn btn-success"></br>
+    </form>
+    
+  </div>
+</div>
+  
+@stop
---
+++ crudTest/resources/views/students/index.blade.php
@@ -0,0 +1,56 @@
+@extends('students.frame')
+@section('content')
+    <div class="container">
+        <div class="row" style="margin:20px;">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h2>CRUD - Estudantes - (Create, Read, Update e Delete)</h2>
+                    </div>
+                    <div class="card-body">
+                        <a href="{{ url('/estudantes/create') }}" class="btn btn-success btn-sm" title="Add New Student">
+                            Adicionar novo!
+                        </a>
+                        <br/>
+                        <br/>
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Nome</th>
+                                        <th>Email</th>
+                                        <th>Celular</th>
+                                        <th>Ações</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($students as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->name }}</td>
+                                        <td>{{ $item->email }}</td>
+                                        <td>{{ $item->phone }}</td>
+  
+                                        <td>
+                                            <a href="{{ url('/estudantes/' . $item->id) }}" title="View Student"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> Ver</button></a>
+                                            <a href="{{ url('/estudantes/' . $item->id . '/edit') }}" title="Edit Student"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Editar</button></a>
+  
+                                            <form method="POST" action="{{ url('/estudantes' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                                {{ method_field('DELETE') }}
+                                                {{ csrf_field() }}
+                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Student" onclick="return confirm("Confirm delete?")"><i class="fa fa-trash-o" aria-hidden="true"></i> Deletar</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+  
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
